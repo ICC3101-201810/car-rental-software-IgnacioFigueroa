9da73bd0e7c3a6d5de837750b6f40cf3f85cd500
ignaciofigueroa <ijfigueroa@miuandes.cl>
2018-04-03 16:56:18 -0300

    SegundaSubida

--- Laboratorio 3/Laboratorio 3/Vehiculo.cs
+++ Laboratorio 3/Laboratorio 3/Vehiculo.cs
@@ -8,5 +8,16 @@ namespace Laboratorio_3
 {
     class Vehiculo
     {
+        int cantidadRuedas;
+        string patente;
+
+
+        public Vehiculo(TipoDeVehiculo miTipo, string acc = "Nada")
+        {
+            string tipo = miTipo;
+            string accesorio = acc;
+
+        }
+            
     }
 }
